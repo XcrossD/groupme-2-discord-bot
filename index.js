4d3286ff9d3a750cfb08bf92df6e32ac79389160
Herbert Lin <herbert.lin.7@gmail.com>
2022-06-15 12:49:56 -0500

    Send groupme image to discord channel

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express')
+const { WebhookClient } = require('discord.js');
+require('dotenv').config()
 const app = express()
 const port = 3000
 
 app.use(express.json())
+
+const webhook = new WebhookClient({ url: process.env.DISCORD_WEBHOOK });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.post('/groupme', (req, res) => {
-  console.log(req.body.attachments)
+  // console.log(req.body.attachments)
+  req.body.attachments.filter(elem => elem.type === 'image')
+    .forEach((elem) => {
+      webhook.send({
+        "embeds": [{
+          "image": {
+            "url": elem.url
+          }
+        }]
+      })
+        .then(console.log)
+        .catch(console.error)
+    })
   res.send('groupme route received post ping')
 })
 
