374aefcae04552de5216dda84c1ef52bae07941a
Herbert Lin <herbert.lin.7@gmail.com>
2022-06-20 14:41:54 -0500

    Make discord correctly parse images by changing filename

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
 const express = require('express')
-const { WebhookClient } = require('discord.js');
+const { default: axios } = require('axios');
+const FormData = require('form-data');
 require('dotenv').config()
 const app = express()
 const port = 3000
 
 app.use(express.json())
 
-const webhook = new WebhookClient({ url: process.env.DISCORD_WEBHOOK });
+const sendFileToWebhook = async (url) => {
+  const filename = url.split('/')
+    .at(-1)
+    .split('.')
+    .slice(0, 2)
+    .join('.')
+  const getResponse = await axios.get(url, {
+    responseType: 'stream'
+  })
+  const form = new FormData()
+  form.append('files[0]', getResponse.data, filename)
+  // console.log(form)
+  axios.post(process.env.DISCORD_WEBHOOK, form)
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/test', (req, res) => {
+  const params = req.body
+  sendFileToWebhook(params.url)
+  // format { url: '' }
+  res.send(params)
 })
 
 app.post('/groupme', (req, res) => {
   // console.log(req.body.attachments)
   req.body.attachments.filter(elem => elem.type === 'image')
     .forEach((elem) => {
-      webhook.send({
-        "embeds": [{
-          "image": {
-            "url": elem.url
-          }
-        }]
-      })
-        .then(console.log)
-        .catch(console.error)
+      sendFileToWebhook(elem.url)
     })
   res.send('groupme route received post ping')
 })
